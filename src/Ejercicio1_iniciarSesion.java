1ad202de577bbf901640b5c8a470205ebd970dc5
AlondraPliego <alondrapliego131104@gmail.com>
2025-09-09 23:53:43 -0600

    Al parecer concluido

--- src/Ejercicio1_iniciarSesion.java
+++ src/Ejercicio1_iniciarSesion.java
@@ -2,4 +2,15 @@ import javax.swing.*;
 
 public class Ejercicio1_iniciarSesion {
     private JPanel Login;
+    private JLabel Logo;
+    private JLabel Mensaje;
+    private JTextField Email;
+    private JPasswordField Contraseña;
+    private JCheckBox checkRecuerdame;
+    private JButton botonIniciarSesion;
+    private JLabel copyright;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
