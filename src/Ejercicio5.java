d5bdcd39189fe492da54c3bca3dc1503060b6f71
Alondra Pliego <alondrapliego131104@gmail.com>
2025-09-09 21:52:31 -0600

    Delete src/Ejercicio5.java

--- src/Ejercicio5.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Ejercicio5 {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JButton cancelarButton;
-    private JButton guardarButton;
-}
